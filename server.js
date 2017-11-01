2680d0b45e7c4af9c92e45601970fb468c24d92a
Mariusz Kosiór <mariusz.kkk@poczta.fm>
2017-11-01 15:50:00 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+var stringifyFile = '';
+
+app.get('/getNote', function (req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+    if (err) throw err;
+    stringifyFile = data
+    res.send(data);
+});
+});
+
+
+app.post('/updateNote/:note', function (req, res) {
+    fs.writeFile('./test.json', stringifyFile += req.params.note, function(err) {
+    if (err) throw err;
+    console.log('file updated');
+});
+});
+
+
+app.listen(3000);
